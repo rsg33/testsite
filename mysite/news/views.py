cc192f4ac6b8232ef15c38159a613debe28518a4
Sergey Rubtsov <rsg81@ya.ru>
2021-08-04 16:58:05 +0300

    добавил фунционал на связную форму

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -27,8 +27,9 @@ def add_news(request):
     if request.method == 'POST':
         form = NewsForm(request.POST)
         if form.is_valid():
-           # print(form.cleaned_data)
-            news =News.objects.create(**form.cleaned_data)
+            # print(form.cleaned_data)
+            # news =News.objects.create(**form.cleaned_data)
+            news = form.save()
             return redirect(news)
     else:
         form = NewsForm
